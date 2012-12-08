01c8d5a985b0cdac28c4a15614a8c37f47fe8918
Simplice Tchoupkoua <simplice.tchoupkoua@adorsys.de>
2012-12-08 15:51:26 +0100

    Initial Commit

---
+++ AudioRecorderApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AudioRecorderApp
+//
+//  Created by Simplice Tchoupkoua on 08.12.12.
+//  Copyright (c) 2012 Simplice. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AudioRecorderApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AudioRecorderApp
+//
+//  Created by Simplice Tchoupkoua on 08.12.12.
+//  Copyright (c) 2012 Simplice. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
